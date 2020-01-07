6510228e35f26251b708a26acd258cb841d9774e
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2020-01-07 12:48:26 -0500

    feat(dal): add method for extract, and map, data from the database

--- dal/database/dbContext.go
+++ dal/database/dbContext.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
+	"github.com/juxemburg/truora_server/apierrors"
 	/*Database dirver import*/
 	_ "github.com/lib/pq"
 )
@@ -14,13 +16,38 @@ type DbContext struct {
 	datasourceName string
 }
 
-/*DbConnection get the connection to the database*/
-func (context DbContext) DbConnection() (*sql.DB, error) {
+func (context DbContext) dbConnection() (*sql.DB, error) {
 	db, err := sql.Open(context.driverName, context.datasourceName)
 	if err != nil {
 		log.Println("error connecting to the database: ", err)
+		return nil, apierrors.NewErrSQL(err.Error())
 	}
-	return db, err
+	return db, nil
+}
+
+/*DbExtraction retrieves an interface based on a SQL statement, alongside an extraction function */
+func (context DbContext) DbExtraction(statement string, extractionFn func(rows *sql.Rows) (r interface{}, err error)) (r interface{}, err error) {
+	db, dberr := context.dbConnection()
+	if dberr != nil {
+		return nil, dberr
+	}
+
+	rows, rowErr := db.Query(statement)
+
+	if rowErr != nil {
+		fmt.Println(rowErr.Error())
+		return nil, apierrors.NewErrSQL(rowErr.Error())
+	}
+	defer rows.Close()
+	result, err := extractionFn(rows)
+	if err != nil {
+		return nil, apierrors.NewErrSQL(rowErr.Error())
+	}
+	if result == nil {
+		return nil, apierrors.NewErrNotFound("The requested resource was not found")
+	}
+
+	return result, nil
 }
 
 func newDbContext(datasourceName string) *DbContext {
