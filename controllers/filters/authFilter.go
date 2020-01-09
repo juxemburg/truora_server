1d4967f880c366becace36480dd5135fe2a129a4
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2020-01-09 09:08:15 -0500

    refactor(filters): AppKey constant name update

--- controllers/filters/authFilter.go
+++ controllers/filters/authFilter.go
@@ -11,12 +11,12 @@ import (
 
 /*AuthFilter filters HTTP request for authentication*/
 func AuthFilter(next http.Handler) http.Handler {
-	if len(common.APP_KEY) == 0 {
+	if len(common.AppKey) == 0 {
 		log.Fatal("the APP_KEY const have not been setted")
 	}
 	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
 		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			return []byte(common.APP_KEY), nil
+			return []byte(common.AppKey), nil
 		},
 		SigningMethod: jwt.SigningMethodHS256,
 	})
